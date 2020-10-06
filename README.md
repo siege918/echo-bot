49a2290df538fa21cdb9f0c2d31e418a8a90e48f
BuildTools <unconfigured@null.spigotmc.org>
2020-10-05 21:49:35 -0700

    Allow loading echo bot configuration from a file, for allowing multiple triggers with different params

--- README.md
+++ README.md
@@ -19,3 +19,26 @@ ECHO_MENTION_ROLE_ID=a_role_id_to_mention_while_echoing
 ```
 
 These variables can be set using either the `-e` flag in the `docker run` command or put in a `.env` file and be used through the `--env-file` command. For ease of use, we are going to use a `.env` file. Once these variables are set properly, run `docker run -it --env-file <path_to_dot_env_file> --name <container_name> echo-bot:latest` to start this bot up.
+
+### Running with a config file
+
+Additionally, this bot can be run by supplying it with a path to a JSON config file, such as the following:
+
+```
+[
+    {
+        "regex": "/testRegex/",
+        "listenChannels": ["channel_id_1", "channel_id_2"],
+        "echoChannel": "channel_id_to_echo_to",
+        "mentionRole": "a_role_id_to_mention_while_echoing"
+    }
+]
+```
+
+When running this way, the following environment variables should be set:
+
+```
+ECHO_TOKEN=discord_token
+ECHO_CHANNELS_CONFIG=path/to/config.json
+```
+
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+const { existsSync, readFileSync } = require("fs");
 const client = new Discord.Client();
 
 const {
@@ -7,18 +8,39 @@ const {
     ECHO_ECHO_CHANNEL_ID,
     ECHO_TOKEN,
     ECHO_MENTION_ROLE_ID,
+    ECHO_CHANNELS_CONFIG
 } = process.env;
 
-const regex = new RegExp(ECHO_LISTEN_REGEX);
-const listenChannels = ECHO_LISTEN_CHANNEL_IDS.split(/,\s?/);
+var channelConfigs = [];
 
-client.on("ready", () => {
-    console.log("Connected.");
-});
+if (!ECHO_CHANNELS_CONFIG) {
+    const regex = new RegExp(ECHO_LISTEN_REGEX);
+    const listenChannels = ECHO_LISTEN_CHANNEL_IDS.split(/,\s?/);
 
-client.on("message", async (message) => {
+    channelConfigs.push({
+        regex,
+        listenChannels,
+        echoChannel: ECHO_ECHO_CHANNEL_ID,
+        mentionRole: ECHO_MENTION_ROLE_ID
+    });
+} else {
+    if (!existsSync(ECHO_CHANNELS_CONFIG)) {
+        throw "Specified config file does not exist";
+    }
+
+    const readChannelConfigs = JSON.parse(readFileSync(ECHO_CHANNELS_CONFIG, "utf8"));
+
+    channelConfigs = readChannelConfigs.map((val) => {
+        return {
+            ...val,
+            regex: new RegExp(val.regex)
+        }
+    });
+}
+
+const tryEcho = (message, echoConfig) => {
     try {
-        if (!listenChannels.includes(message.channel.id)) {
+        if (!echoConfig.listenChannels.includes(message.channel.id)) {
             return;
         }
 
@@ -26,20 +48,30 @@ client.on("message", async (message) => {
             return;
         }
 
-        if (!regex.test(message.content)) {
+        if (!echoConfig.regex.test(message.content)) {
             return;
         }
 
-        const echoChannel = client.channels.get(ECHO_ECHO_CHANNEL_ID);
+        const echoChannel = client.channels.get(echoConfig.echoChannel);
 
-        if (ECHO_MENTION_ROLE_ID) {
+        if (echoConfig.mentionRole) {
             echoChannel.send(
-                `Echoing for <@&${ECHO_MENTION_ROLE_ID}>: ${message.content}`
+                `Echoing for <@&${echoConfig.mentionRole}>: ${message.content}`
             );
         } else {
             echoChannel.send(`Echoing: ${message.content}`);
         }
-    } catch (e) {}
+    } catch (e) { }
+};
+
+client.on("ready", () => {
+    console.log("Connected.");
+});
+
+client.on("message", async (message) => {
+    for (const echoConfig of channelConfigs) {
+        tryEcho(message, echoConfig);
+    }
 });
 
 client.login(ECHO_TOKEN);
